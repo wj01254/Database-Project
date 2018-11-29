46ba13905acf61d0e9eb83cc5b968f56c9a573b6
wende <wende@10.40.21.234>
2018-11-28 23:24:01 -0500

    Connection successful

--- Database/src/DBAccess.java
+++ Database/src/DBAccess.java
@@ -1,45 +1,51 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class DBAccess {
-	
-	public static void main (String[] args) {
+
+	public static void main (String[] args) throws ClassNotFoundException {
 		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String connectionurl = "jdbc:mysql://localhost:3306;" + "databaseName=master;" + "user=MyUserName;password=**;"; 
-			Connection con = DriverManager.getConnection(connectionurl);
-			//2. Create a statement
-			Statement myStmt = con.createStatement();
-			//3. Execute SQL query
-			ResultSet myRs = myStmt.executeQuery("select * from Student");
-			//4. Process the result set
-			while (myRs.next()) {
-				System.out.println(myRs.getString("First Name") + ", " + 
-			myRs.getString("Last Name") + ", " + myRs.getInt("Year") + ", " + myRs.getString("School") 
-			+ ", " + myRs.getInt("DOB") + ", " + myRs.getString("Address"));
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		String connectionurl = "jdbc:sqlserver://localhost:1433;" + "database=master;user=jdbc;password=abc;";
+
+		try (Connection con = DriverManager.getConnection(connectionurl); Statement stmt = con.createStatement();){
+			String SQL = "select* from Student";
+			ResultSet rs = stmt.executeQuery(SQL);
+
+			while (rs.next()) {
+				System.out.println(rs.getInt("StudentID") + "\t " +rs.getString("FirstName") + "\t" + rs.getString("LastName") + "\t" + rs.getString("Year")
+				+ "\t" + rs.getString("School") + "\t" + rs.getDate("DOB") + "\t" + rs.getString("Address"));
+				
+				
 			}
+			con.close();
 		}
-		catch(Exception e) {
+		catch(SQLException e) {
 			e.printStackTrace();
 		}
+
+
+
 	}
-	
 
-	public static Connection getDataBaseConnection() throws SQLException {
-
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Statement state = connection.createStatement();
-        } catch (ClassNotFoundException ex) {
-            System.out.println("driver not found" + ex);
-        }
-        Connection connection = null;
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/master", "DESKTOP-HFN08GM\\wende" , "");
-        System.out.println("Stuck?");
-        return  connection;
-        
-        
-    }
-
+	//	public static Connection getDataBaseConnection() throws SQLException {
+	//
+	//        try {
+	//            //Class.forName("com.mysql.jdbc.Driver");
+	//        	Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+	//        
+	//        } catch (ClassNotFoundException ex) {
+	//            System.out.println("driver not found" + ex);
+	//        }
+	//        
+	//        System.out.println("Stuck?");
+	//       	
+	//        	
+	//        	 return  con;
+	//        
+	//    }
 }
